589a75584ca4e68c612cc81df7141f8a1efe7a3a
anwittin <anwittin@gmail.com>
2015-10-13 14:52:57 -0700

    Database exercises
    simple database exercises from learn python in a day (2).

---
+++ LPIAD2/sql_exercises6.py
@@ -0,0 +1,22 @@
+import sqlite3
+# Connect to database 'simpsons.db'
+conn = sqlite3.connect('simpsons.db')
+
+# Update a character
+conn.execute("UPDATE SIMPSON_INFO set AGE = 41 where NAME = 'Homer Simpson'")
+
+# Save changes
+conn.commit()
+
+# Prints the number of changes to the database
+
+changes = conn.total_changes
+print "Number of changes: ", changes
+conn.execute("DROP TABLE SIMPSON_INFO")
+
+cursor = conn.execute("SELECT * from SIMPSON_INFO")
+
+# Get data from cursor
+
+rows = cursor.fetchall()
+print rows
