3bcac25a2e21945c289d5f2aaba391debfdafed5
anwittin <anwittin@gmail.com>
2015-10-26 16:12:21 -0700

    LPTHW_game

---
+++ LPTHW/Game_1.py
@@ -0,0 +1,23 @@
+# Make a game that has 4 rooms, 2 traps, 2 monsters
+# 3 keys, 1 treasure and an exit.
+
+
+
+
+
+# Room 1 (Start Room, 1 key, 1 trap)
+
+
+
+
+# Room 2 (1 key, 1 monster)
+
+
+
+
+# Room 3 (1 key, 1 trap)
+
+
+
+
+# Room 4 (1 boss monster, 1 treasure and an exit)
