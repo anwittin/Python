9811535eb149410cc6db00474999595137a9212d
Tony Wittinger <anwittin@gmail.com>
2015-11-06 11:00:26 -0800

    sync

---
+++ Game_drill.py
@@ -0,0 +1,63 @@
+import time
+import random
+apples = 0
+gold = 0
+
+def start():
+	print("Hello and welcome!")
+	name = input("What's your name: ").title()
+	print("Welcome, {}!".format(name))
+	print("The object of this game is to collect apples.")
+	print("After collecting the apples you sell them")
+	choice = input("Do you want to play Y/N \n:>").upper()
+	if choice == "Y":
+		begin()
+	if choice == "N":
+		print("Okay Seeya!")
+def begin():
+	global apples
+	global gold
+	
+	print("Let's get started!")
+	pick = input("Do you want to pick an apple Y/N?\n:>").upper()
+	if pick == "Y":
+		#time.sleep(.1)
+		print("You pick an apple.")
+		apples += 1  
+		print("You currently have, {} apples".format(apples))
+		begin()
+	if pick == "N":
+		sell()
+		
+def sell():
+	global gold
+	global apples
+	
+	sell = input("Do you want to sell your apples? Y/N\n:>").upper()
+	if sell == "Y":
+		print("You currently have, {} apples".format(apples))
+		print("You've sold your apples.")
+		randnum = random.randrange(1, 25)
+		gold = apples *randnum
+		apples = 0
+		print("The current market value of apples is {} gold.".format(randnum))
+		print("You now have {} gold".format(gold))
+	if sell =="N":
+		print("I'm sorry the point of the game is to make money selling apples. Try again another time.")
+		return
+	if gold >= 99:
+		gold = 0
+		print("You won the game!")
+		play = input("Do you want to play again? Y/N\n:>").upper()
+		if play == "Y":
+			begin()
+		if play == "N":
+			print("Congrats, Have a good day!")
+			
+	elif gold <= 98:  
+		play = input("Bummer not enough gold to win try again? Y/N\n:>").upper()
+		if play == "Y":
+			begin()
+		if play == "N":
+			print("Okay Seeya!")
+start()
--- LPIAD2/GUI_Database_App.py
+++ LPIAD2/GUI_Database_App.py
@@ -17,37 +17,119 @@ class Frame(wx.Frame):
 
         self.CreateStatusBar()
 
-        #Setup add new character ui
-        #Create static box
-        wx.StaticBox(panel, label='Add a new character', pos=(20,40), size=(280,190))
+        # Setup add new character ui
+        # Create static box
+        wx.StaticBox(panel, label='Add a new character', pos=(20, 30), size=(280, 190))
 
-        #text for name, gender, etc
-        wx.StaticText(panel, label='Name:', pos=(30, 70))
-        wx.StaticText(panel, label='Gener:', pos=(30, 110))
-        wx.StaticText(panel, label='Age:', pos=(30, 150))
-        wx.StaticText(panel, label='Occupation:', pos=(30, 190))
+        # text for name, gender, etc
+        wx.StaticText(panel, label='Name:', pos=(30, 60))
+        wx.StaticText(panel, label='Gender:', pos=(30, 90))
+        wx.StaticText(panel, label='Age:', pos=(30, 120))
+        wx.StaticText(panel, label='Occupation:', pos=(30, 150))
 
-        #Single line text boxes
-        self.sName = wx.TextCtrl(panel, size=(150, -1), pos=(130, 70))
-        self.sGen = wx.TextCtrl(panel, size=(150, -1), pos=(130, 110))
-        self.sAge = wx.TextCtrl(panel, size=(150, -1), pos=(130, 150))
-        self.sOcc = wx.TextCtrl(panel, size=(150, -1), pos=(130, 190))
+        # Single line text boxes
+        self.sName = wx.TextCtrl(panel, size=(150, -1), pos=(130, 60))
+        self.sGen = wx.TextCtrl(panel, size=(150, -1), pos=(130, 90))
+        self.sAge = wx.SpinCtrl(panel, value='0', size=(70, 25), pos=(130, 120))
+        self.sOcc = wx.TextCtrl(panel, size=(150, -1), pos=(130, 150))
 
         # Save button
-        save = wx.Button(panel, label="Add Character", pos=(100, 230))
-        save.Bind(wx.EVT_BUTTON, self.addCharacter)
+        save = wx.Button(panel, label="Create Character", pos=(100, 185))
+        save.Bind(wx.EVT_BUTTON, self.updateCharacter)
 
-    def exitProgram(self, event):
-        self.Destroy()
+
+        # Setup update character
+
+        wx.StaticBox(panel, label='Add Character', pos=(20, 240), size=(280, 190))
+
+        # text for name, gender, etc
+        wx.StaticText(panel, label='Name:', pos=(30, 270))
+        wx.StaticText(panel, label='Gender:', pos=(30, 300))
+        wx.StaticText(panel, label='Age:', pos=(30, 330))
+        wx.StaticText(panel, label='Occupation:', pos=(30, 360))
+
+        # Single line text boxes
+        self.sName = wx.TextCtrl(panel, size=(150, -1), pos=(130, 270))
+        self.sGen = wx.TextCtrl(panel, size=(150, -1), pos=(130, 300))
+        self.sAge = wx.SpinCtrl(panel, value='0', size=(70, 25), pos=(130, 330))
+        self.sOcc = wx.TextCtrl(panel, size=(150, -1), pos=(130, 360))
+
+        # Save button
+        save = wx.Button(panel, label="Update Character", pos=(100, 420))
+        save.Bind(wx.EVT_BUTTON, self.updateCharacter)
+
+
+
+        # Setup the Table UI
+        # Setup table as listCtrl
+        self.listCtrl = wx.ListCtrl(panel, size=(400,400), pos=(350,40), style=wx.LC_REPORT | wx.BORDER_SUNKEN)
+
+        # Add Columns to the listCtrl
+        self.listCtrl.InsertColumn(0, "ID")
+        self.listCtrl.InsertColumn(1, "Name")
+        self.listCtrl.InsertColumn(2, "Gender")
+        self.listCtrl.InsertColumn(3, "Age")
+        self.listCtrl.InsertColumn(4, "Occupation")
+
+        self.fillListCtrl()
+        # Run onSelect funciton when item is selected
+        self.listCtrl.Bind(wx.EVT_LIST_ITEM_SELECTED, self.onSelect)
+
+        # Delete Button
+        deleteBtn = wx.Button(panel, label="Delete", pos=(640, 450))
+
+        # Bind the delete button to onDelete functino
+        deleteBtn.Bind(wx.EVT_BUTTON, self.onDelete)
+
+
     def addCharacter(self, event):
         name = self.sName.GetValue()
         gen = self.sGen.GetValue()
         age = self.sAge.GetValue()
         occ = self.sOcc.GetValue()
 
-        # Adding character to database
+
+        # Checking if variables have values
+        if (name == '') or (gen == '') or (age == '') or (occ == ''):
+            # Alert the user a variable is empty
+            dlg = wx.MessageDialog(None, 'Some character details are missing. Please enter values in each text'
+                                         'box.', 'Missing Details', wx.OK)
+            dlg.ShowModal()
+            dlg.Destroy()
+            return False
+
+    # Adding character to database
         GUI_database.newCharacter(name, gen, age, occ)
         print GUI_database.viewAll()
+        # Clear boxes
+        self.sName.Clear()
+        self.sGen.Clear()
+        self.sAge.SetValue(0)
+        self.sOcc.Clear()
+
+
+        self.fillListCtrl()
+    def exitProgram(self, event):
+        self.Destroy()
+
+    def fillListCtrl(self):
+
+        allData = GUI_database.viewAll()
+        self.listCtrl.DeleteAllItems()
+        for row in allData:
+            self.listCtrl.Append(row)
+
+    def onSelect(self, event):
+        self.onSelectId = event.GetText()
+
+    def onDelete(self, event):
+        # Delete the character
+        GUI_database.deleteCharacter(self.onSelectId)
+        self.fillListCtrl()
+    def updateCharacter(self, event):
+        pass
+
+
 
 app = wx.App()
 frame = Frame("Python GUI")
