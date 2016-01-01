1a9eeba502dc74b0279fab8466f6d088a6fc425f
Tony Wittinger <anwittin@gmail.com>
2016-01-01 14:40:11 -0800

    updates

---
+++ Coursera/test emaildb.py
@@ -0,0 +1,8 @@
+fname = raw_input('Enter file name: ')
+if ( len(fname) < 1 ) : fname = 'mbox.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From: ') : continue
+    pieces = line.split().find("@")
+    org = pieces[1]
+    print email
